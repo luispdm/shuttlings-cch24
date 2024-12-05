3352ec12f845944a01d97fb2494b3b596e1cd168
luispdm <17044119+luispdm@users.noreply.github.com>
2024-12-05 17:47:16 +0100

    ipv6 day 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,4 +1,4 @@
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 use axum::extract::Query;
 
@@ -12,6 +12,18 @@ pub struct AddV4 {
 pub struct SubV4 {
     from: Ipv4Addr,
     to: Ipv4Addr,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct AddV6 {
+    from: Ipv6Addr,
+    key: Ipv6Addr,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct SubV6 {
+    from: Ipv6Addr,
+    to: Ipv6Addr,
 }
 
 pub async fn dest_v4(from_key: Query<AddV4>) -> String {
@@ -34,4 +46,30 @@ pub async fn key_v4(from_to: Query<SubV4>) -> String {
         .map(|(t, f)| t.wrapping_sub(f).to_string())
         .collect::<Vec<String>>()
         .join(".")
+}
+
+pub async fn dest_v6(from_key: Query<AddV6>) -> String {
+    let segments: [u16; 8] = from_key
+        .from
+        .segments()
+        .iter()
+        .zip(from_key.key.segments())
+        .map(|(f, k)| f ^ k)
+        .collect::<Vec<u16>>()
+        .try_into()
+        .unwrap();
+    Ipv6Addr::from(segments).to_string()
+}
+
+pub async fn key_v6(from_to: Query<SubV6>) -> String {
+    let segments: [u16; 8] = from_to
+        .to
+        .segments()
+        .iter()
+        .zip(from_to.from.segments())
+        .map(|(t, f)| t ^ f)
+        .collect::<Vec<u16>>()
+        .try_into()
+        .unwrap();
+    Ipv6Addr::from(segments).to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/", get(hello_world))
         .route("/-1/seek", get(seek))
         .route("/2/dest", get(dest_v4))
-        .route("/2/key", get(key_v4));
+        .route("/2/key", get(key_v4))
+        .route("/2/v6/dest", get(dest_v6))
+        .route("/2/v6/key", get(key_v6));
 
     Ok(router.into())
 }
